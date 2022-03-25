114058c770313aae624f680fcde08021446971f5
Luka Vuk <luka.vuk@datamir.info>
2022-03-25 17:35:14 +0100

    test commit

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import "./HeaderOption.css"
-import {Avatar} from "@material-ui/core"
+import React from "react";
+import "./HeaderOption.css";
+import { Avatar } from "@material-ui/core";
 
-export default function HeaderOption({Icon, title, avatar}) {
+export default function HeaderOption({ Icon, title, avatar }) {
   return (
     <div className="headerOption">
-        {/* If I pass the Icon, only then should I render Icons itself as a component*/}
-        {Icon &&
-            <Icon className="headerOption__" />}
-            
-        {avatar && (
-            <Avatar className="headerOption__icon" src={avatar} />
-        )}
-        <h3 className="headerOption__title">{title}</h3>
+      {/* If I pass the Icon, only then should I render Icons itself as a component*/}
+
+      {/* Dummy commit*/}
+      {Icon && <Icon className="headerOption__" />}
+
+      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
+      <h3 className="headerOption__title">{title}</h3>
     </div>
-  )
+  );
 }
